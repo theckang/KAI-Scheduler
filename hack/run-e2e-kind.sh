53b4a08af517b198272285687fa20024b9cd766d
davidLif <davidshani12@gmail.com>
2025-04-01 17:02:32 +0300

    Change e2e example helm path (#28)

--- hack/run-e2e-kind.sh
+++ hack/run-e2e-kind.sh
@@ -10,23 +10,20 @@ KIND_CONFIG=${REPO_ROOT}/hack/e2e-kind-config.yaml
 GOPATH=${HOME}/go
 GOBIN=${GOPATH}/bin
 
-NVCR_SECRET_FILE_PATH=${1}
-if [ -z "$NVCR_SECRET_FILE_PATH" ]; then
-    echo "Must a path to an appropriate secret file, that contains the credentials for the nvstaging-runai helm and docker image repository"
-    exit 1
-fi
-
 kind create cluster --config ${KIND_CONFIG} --name $CLUSTER_NAME
 
-kubectl create namespace kai-scheduler
-# Set an appropriate secret to allow the kube-ai system pods to pull from nvstaging-runai and pull test images for the e2e tests
-kubectl apply -f ${NVCR_SECRET_FILE_PATH} -n kai-scheduler
-
+# Add relevant helm repository
+helm repo add nvidia https://helm.ngc.nvidia.com/nvidia/k8s
+helm repo update
 
 # Install the fake-gpu-operator to provide a fake GPU resources for the e2e tests
 helm upgrade -i gpu-operator fake-gpu-operator/fake-gpu-operator --namespace gpu-operator --create-namespace --version 0.0.53 --set topology.nodePools.default.gpuCount=8
 
-helm upgrade -i kai-scheduler nvstaging-runai/kai-scheduler -n kai-scheduler --create-namespace --set "global.imagePullSecrets[0].name=nvcr-secret" --set "global.gpuSharing=true" --set "global.registry=nvcr.io/nvstaging/runai" --version v0.2.0
+# install third party operators to check the compatibility with the kai-scheduler
+${REPO_ROOT}/hack/third_party_integrations/deploy_ray.sh
+${REPO_ROOT}/hack/third_party_integrations/deploy_kubeflow.sh
+
+helm upgrade -i kai-scheduler nvidia/kai-scheduler -n kai-scheduler --create-namespace --set "global.registry=nvcr.io/nvidia/k8s" --set "global.gpuSharing=true"
 
 # Allow all the pods in the fake-gpu-operator and kai-scheduler to start
 sleep 30
